6a45da3c5012f8a9498e26dbe3de941c2ef40e20
yuri <ykr337@gmail.com>
2017-10-19 21:44:05 -0200

    total de eventos por cliente

--- src/main/java/br/com/segware/AnalizadorRelatorioImpl.java
+++ src/main/java/br/com/segware/AnalizadorRelatorioImpl.java
@@ -2,32 +2,41 @@ package br.com.segware;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class AnalizadorRelatorioImpl implements IAnalisadorRelatorio{
-	
+public class AnalizadorRelatorioImpl implements IAnalisadorRelatorio {
+
 	private RelatorioDAO relatorioDAO;
-	
+
 	public AnalizadorRelatorioImpl() {
-		try{
-			this.relatorioDAO = new RelatorioDAO("/home/yuri/Documentos/segware/developer-test-file-analyze/src/test/java/br/com/segware/relatorio.csv");			
-		}catch (FileNotFoundException e) {
+		try {
+			this.relatorioDAO = new RelatorioDAO("/home/yuri/Documentos/segware/developer-test-file-analyze/src/test/java/br/com/segware/relatorio.csv");
+		} catch (FileNotFoundException e) {
 			System.out.print(e.getMessage());
 		}
 	}
 
 	@Override
 	public Map<String, Integer> getTotalEventosCliente() {
-		try{
-			List<Evento> arquivo = this.relatorioDAO.readFile();			
-			Map<String, Integer> totalEventos= arquivo.stream().collect(Collectors.groupingBy(Evento -> codCliente, Collectors.counting());
-		}catch(Exception e){
+		Map<String, Integer> totalEventos = new HashMap<>();
+		try {
+			List<Evento> eventos = this.relatorioDAO.readFile();
+			for (Evento evento : eventos) {
+				if (totalEventos.containsKey(evento.getCodCliente())) {
+					totalEventos.put(evento.getCodCliente(), totalEventos.get(evento.getCodCliente()) + 1);
+				} else {
+					totalEventos.put(evento.getCodCliente(), 1);
+
+				}
+			}
+		} catch (Exception e) {
 			System.out.print(e.getMessage());
 		}
-		
-		return null;
+		return totalEventos;
 	}
 
 	@Override
--- src/main/java/br/com/segware/Evento.java
+++ src/main/java/br/com/segware/Evento.java
@@ -7,6 +7,54 @@ public class Evento {
 	
 	private String coseq;
 	private String codCliente;
+	public String getCoseq() {
+		return coseq;
+	}
+
+	public void setCoseq(String coseq) {
+		this.coseq = coseq;
+	}
+
+	public String getCodCliente() {
+		return codCliente;
+	}
+
+	public void setCodCliente(String codCliente) {
+		this.codCliente = codCliente;
+	}
+
+	public Tipo getTipoEvento() {
+		return tipoEvento;
+	}
+
+	public void setTipoEvento(Tipo tipoEvento) {
+		this.tipoEvento = tipoEvento;
+	}
+
+	public Date getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(Date dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public Date getDataFim() {
+		return dataFim;
+	}
+
+	public void setDataFim(Date dataFim) {
+		this.dataFim = dataFim;
+	}
+
+	public String getCodAtendente() {
+		return codAtendente;
+	}
+
+	public void setCodAtendente(String codAtendente) {
+		this.codAtendente = codAtendente;
+	}
+
 	private Tipo tipoEvento;
 	private Date dataInicio;
 	private Date dataFim;

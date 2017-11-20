4749fb19f1eb38b6ca4c76235e7394e5e1e096b0
tglima <thg.limadesousa@gmail.com>
2017-11-20 09:54:03 -0200

    Implementa os métodos solicitados

--- src/test/java/br/com/segware/AnalisadorRelatorioTest.java
+++ src/test/java/br/com/segware/AnalisadorRelatorioTest.java
@@ -10,41 +10,42 @@ import org.junit.Test;
 
 public class AnalisadorRelatorioTest {
 
-    IAnalisadorRelatorio analisador;
+	IAnalisadorRelatorio analisador;
 
-    @Before
-    public void before() throws IOException {
-        // analisador = sua implementação
-    }
+	@Before
+	public void before() throws IOException {
+		analisador = new Analisador();
+	}
 
-    @Test
-    public void totalDeEventosDoCliente0001() {
-        assertEquals(7, analisador.getTotalEventosCliente().get("0001"), 0);
-    }
+	@Test
+	public void totalDeEventosDoCliente0001() {
+		assertEquals(7, analisador.getTotalEventosCliente().get("0001"), 0);
+	}
 
-    @Test
-    public void totalDeEventosDoCliente0003() {
-        assertEquals(3, analisador.getTotalEventosCliente().get("0003"), 0);
-    }
+	@Test
+	public void totalDeEventosDoCliente0003() {
+		assertEquals(3, analisador.getTotalEventosCliente().get("0003"), 0);
+	}
 
-    @Test
-    public void tempoMedioDeAtendimentoEmSegundosDoAtendenteAT01() {
-        assertEquals(159, analisador.getTempoMedioAtendimentoAtendente().get("AT01"), 0);
-    }
+	@Test
+	public void tempoMedioDeAtendimentoEmSegundosDoAtendenteAT01() {
+		assertEquals(159, analisador.getTempoMedioAtendimentoAtendente().get("AT01"), 0);
+	}
 
-    @Test
-    public void tempoMedioDeAtendimentoEmSegundosDoAtendenteAT02() {
-        assertEquals(156, analisador.getTempoMedioAtendimentoAtendente().get("AT02"), 0);
-    }
+	@Test
+	public void tempoMedioDeAtendimentoEmSegundosDoAtendenteAT02() {
+		assertEquals(156, analisador.getTempoMedioAtendimentoAtendente().get("AT02"), 0);
+	}
 
-    @Test
-    public void tipoComMaisEventos() {
-        assertArrayEquals(new Tipo[] { Tipo.ALARME, Tipo.DESARME, Tipo.TESTE, Tipo.ARME },
-                analisador.getTiposOrdenadosNumerosEventosDecrescente().toArray(new Tipo[Tipo.values().length]));
-    }
+	@Test
+	public void tipoComMaisEventos() {
+		assertArrayEquals(new Tipo[] { Tipo.ALARME, Tipo.DESARME, Tipo.TESTE, Tipo.ARME }, analisador
+				.getTiposOrdenadosNumerosEventosDecrescente().toArray(new Tipo[Tipo.values().length]));
+	}
 
-    @Test
-    public void identificarEvento() {
-        assertArrayEquals(new Integer[] { 7 }, analisador.getCodigoSequencialEventosDesarmeAposAlarme().toArray(new Integer[1]));
-    }
+	@Test
+	public void identificarEvento() {
+		assertArrayEquals(new Integer[] { 7 },
+				analisador.getCodigoSequencialEventosDesarmeAposAlarme().toArray(new Integer[1]));
+	}
 }
